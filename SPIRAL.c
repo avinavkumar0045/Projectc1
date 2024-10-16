b341153967c17d5539cb5344f680d9eda3c87215
avinavkumar0045 <avinavkumar0045@gmail.com>
2024-10-16 17:35:45 +0530

    Update SPIRAL.c

--- SPIRAL.c
+++ SPIRAL.c
@@ -1,12 +1,5 @@
 #include<stdio.h> // WAVE PRINT
-/* min r
-   minr ++,
-   max c
-   max c--,
-   max r[reverse]
-   max r --,
-   min c[reverse]
-   min c ++,*/  // STOP when count = row * col
+
 int  main() {
     int a[5][6] ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
     int r = 5;
